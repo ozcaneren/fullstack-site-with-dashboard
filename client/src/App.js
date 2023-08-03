ce5d76ce31f4a012116cafbfdf80c69f5c60733f
eren ozcan <erenozcandev@gmail.com>
2023-08-04 01:57:50 +0300

    update app.js & install tailwindcss, ui library etc..

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import List from './pages/List';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <List />
+    </>
   );
 }
 
