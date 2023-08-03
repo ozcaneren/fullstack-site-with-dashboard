b114b2f253eacfddaebd56255ef54ad3a25c0fbd
eren ozcan <erenozcandev@gmail.com>
2023-08-04 01:55:15 +0300

    add delete function & update fetchmark file

--- client/src/api/EditMark.js
+++ client/src/api/EditMark.js
@@ -1,71 +1,141 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import EditMark from "./EditMark";
+import { HiOutlineTrash } from "react-icons/hi";
+import { AiOutlineEdit } from "react-icons/ai";
 
-const EditMark = ({ markId, onClose, onUpdate }) => {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
-  const [description, setDescription] = useState('');
+const FetchMarks = ({ shouldFetch }) => {
+  const [marks, setMarks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchMarkData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/api/marks/${markId}`);
-        const markData = response.data.data;
-        setTitle(markData.title);
-        setUrl(markData.url);
-        setDescription(markData.description);
-      } catch (error) {
-        console.error('Error fetching mark data:', error);
-      }
-    };
+  const [editMode, setEditMode] = useState(false);
+  const [selectedMarkId, setSelectedMarkId] = useState(null);
 
-    fetchMarkData();
-  }, [markId]);
-
-  const handleUpdate = async (e) => {
-    e.preventDefault();
-
+  const getMarks = async () => {
     try {
-      const updatedMark = {
-        title: title,
-        url: url,
-        description: description,
-      };
-
-      await axios.put(`http://localhost:5000/api/marks/${markId}`, updatedMark);
-      
-      onUpdate();
-      
-      onClose();
+      setIsLoading(true);
+      const response = await axios.get("http://localhost:5000/api/marks");
+      setMarks(response.data.data);
     } catch (error) {
-      console.error('Error updating mark:', error);
+      console.error("Error fetching marks:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  
+  useEffect(() => {
+    getMarks();
+  }, [shouldFetch]);
+
+  const handleEdit = (markId) => {
+    setSelectedMarkId(markId);
+    setEditMode(true);
+  };
+
+  const handleDelete = async (markId) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/marks/${markId}`);
+      getMarks();
+    } catch (error) {
+      console.error("Error deleting mark:", error);
+    }
+  };
 
   return (
-    <div>
-      <form onSubmit={handleUpdate}>
-        <div className="flex justify-center items-center mx-auto mt-4">
-          <div className="flex flex-col">
-            <div className="rounded-lg">
-              <div className="min-w-full">
-                <div className="overflow-hidden">
-                  <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className=" px-4 py-1 h-12 text-gray-800 rounded-l-lg border border-gray-300 focus:outline-none" placeholder="Title" />
-                  <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}  className="px-4 py-1 h-12 text-gray-800 rounded-x-lg border border-gray-300 focus:outline-none" placeholder="Description" />
-                  <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} className=" px-4 py-1 h-12 text-gray-800 rounded-x-lg border border-gray-300 focus:outline-none" placeholder="URL" />
-                  <button type="submit" className="bg-blue-500 px-4 py-2 h-12 w-[57px] text-white rounded-r-lg">
-                    +
-                  </button>
-                </div>
+    <div className="p-4">
+      <h2 className="text-center text-3xl">My bookmark app</h2>
+      <div className="max-w-3xl mx-auto mt-4">
+        <div className="flex flex-col">
+          <div className="overflow-x-auto shadow-md sm:rounded-lg">
+            <div className="inline-block min-w-full align-middle">
+              <div className="overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200 table-fixed">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase "
+                      >
+                        Title
+                      </th>
+                      <th
+                        scope="col"
+                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase "
+                      >
+                        Description
+                      </th>
+                      <th
+                        scope="col"
+                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase "
+                      >
+                        URL
+                      </th>
+                      <th scope="col" className="p-4">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                      <th scope="col" className="p-4">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  {marks.map((mark, index) => (
+                    <tbody
+                      key={index}
+                      className="bg-white divide-y divide-gray-200"
+                    >
+                      <tr className="hover:bg-gray-100">
+                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap ">
+                          {mark.title}
+                        </td>
+                        <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap ">
+                          {mark.description}
+                        </td>
+                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                          <a
+                            href={`https://${mark.url}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {mark.url}
+                          </a>
+                        </td>
+                        <td className="text-left whitespace-nowrap">
+                          <button
+                            onClick={() => handleEdit(mark._id)}
+                            href="#"
+                            className="text-blue-600 hover:underline"
+                          >
+                            <AiOutlineEdit />
+                          </button>
+                        </td>
+                        <td className="whitespace-nowrap">
+                          <button
+                            onClick={() => handleDelete(mark._id)}
+                            href="#"
+                            className="text-blue-600 hover:underline"
+                          >
+                            <HiOutlineTrash />
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  ))}
+                </table>
               </div>
             </div>
           </div>
-        </div> 
-      </form>
+        </div>
+      </div>
+      {editMode && (
+        <EditMark
+          markId={selectedMarkId}
+          onClose={() => setEditMode(false)}
+          onUpdate={getMarks}
+        />
+      )}
+      {isLoading && <p className="text-center text-4xl">Loading...</p>}
     </div>
   );
 };
 
-export default EditMark;
+export default FetchMarks;
--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
-import FetchMarks from '../api/FetchMarks';
+import React, { useState } from 'react'
+import FetchMarks from '../api/FetchMarks'
+import AddMark from '../api/AddMark'
+
 
 function List() {
-  
+
+  const [shouldFetchMarks, setShouldFetchMarks] = useState(false);
+
+  const handleAddMark = () => {
+    // Toggle the shouldFetchMarks state to trigger a refresh
+    setShouldFetchMarks(!shouldFetchMarks);
+  };
+
   return (
     <div>
-      <FetchMarks />
+      <FetchMarks shouldFetch={shouldFetchMarks} /> {/* Pass shouldFetchMarks as a prop */}
+      <AddMark onAddMark={handleAddMark} /> {/* Pass handleAddMark as a prop */}
     </div>
   )
 }
 
-export default List;
+export default List
